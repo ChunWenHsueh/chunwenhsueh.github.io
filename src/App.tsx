7bab37adacbaffcc5cc7cca8bc4831dc7b998626
Chun-Wen <hsueh.ch@northeastern.edu>
2024-11-05 08:11:05 -0800

    Updated resume and adjust ui

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import Footer from "./components/my_ui/Footer";
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       {NavBar()}
-      <div className="flex h-screen snap-y snap-mandatory flex-col overflow-y-scroll">
-        <div className="h-screen snap-center">{Hero()}</div>
-        <div className="h-screen snap-center">{Experience()}</div>
-        <div className="relative flex h-screen snap-center flex-col">
+      <div className="flex h-screen flex-col">
+        <div className="h-screen">{Hero()}</div>
+        <div className="h-screen">{Experience()}</div>
+        <div className="relative flex h-screen flex-col">
           {Project()}
           <div className="absolute inset-x-0 bottom-0">{Footer()}</div>
         </div>
--- src/components/my_ui/Footer.tsx
+++ src/components/my_ui/Footer.tsx
@@ -2,25 +2,30 @@ import { FaGithub, FaLinkedin, FaFileDownload } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="flex w-full items-center justify-center space-x-2 bg-[#f2f3f4] p-4 dark:bg-neutral-800">
-      <a
-        className="flex items-center transition duration-200 hover:scale-110 hover:ease-out active:scale-90 active:ease-out md:text-xl xl:text-3xl"
-        href="https://github.com/ChunWenHsueh"
-      >
-        <FaGithub className="h-6 w-6" />
-      </a>
-      <a
-        className="flex items-center transition duration-200 hover:scale-110 hover:ease-out active:scale-90 active:ease-out md:text-xl xl:text-3xl"
-        href="https://www.linkedin.com/in/chun-wen-hsueh/"
-      >
-        <FaLinkedin className="h-6 w-6" />
-      </a>
-      <a
-        className="flex items-center transition duration-200 hover:scale-110 hover:ease-out active:scale-90 active:ease-out md:text-xl xl:text-3xl"
-        href="./Chun_Wen_Hsueh_Resume.pdf"
-      >
-        <FaFileDownload className="h-6 w-6" />
-      </a>
+    <footer className="flex w-full items-center justify-between space-x-2 bg-[#f2f3f4] p-4 dark:bg-neutral-800">
+      <div className="flex items-center justify-center space-x-2">
+        <a
+          className="flex items-center transition duration-200 hover:scale-110 hover:ease-out active:scale-90 active:ease-out md:text-xl xl:text-3xl"
+          href="https://github.com/ChunWenHsueh"
+        >
+          <FaGithub className="h-6 w-6" />
+        </a>
+        <a
+          className="flex items-center transition duration-200 hover:scale-110 hover:ease-out active:scale-90 active:ease-out md:text-xl xl:text-3xl"
+          href="https://www.linkedin.com/in/chun-wen-hsueh/"
+        >
+          <FaLinkedin className="h-6 w-6" />
+        </a>
+        <a
+          className="flex items-center transition duration-200 hover:scale-110 hover:ease-out active:scale-90 active:ease-out md:text-xl xl:text-3xl"
+          href="./Chun_Wen_Hsueh_Resume.pdf"
+        >
+          <FaFileDownload className="h-6 w-6" />
+        </a>
+      </div>
+      <div>
+        <p>Copyright © 2024 Chun-Wen Hsueh. All Rights Reserved.</p>
+      </div>
     </footer>
   );
 }
